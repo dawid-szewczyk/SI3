f3d9145ee52e7c7edab57221602c1f4f78f788f8
Grzegorz23 <gieem17@gmail.com>
2018-05-24 01:44:16 +0200

    testy i turniej

--- SI3/Program.cs
+++ SI3/Program.cs
@@ -12,11 +12,13 @@ namespace SI3
 {
     class Program
     {
+       
         static void Main(string[] args) {
-            Board board = new Board(5);
+            int times=0;
+            Board board = new Board(6);
             List<Player> players = new List<Player> {
-                new AIPlayer(1, 4, new PointsEnemyDisadvantage(), new OuterToInner(), new  AlphaBeta(), board),
-                new AIPlayer(2, 4, new PointsAdvantage(),  new OuterToInner(), new  AlphaBeta(), board)
+                new AIPlayer(1, 4, new PointsGain(), new ClassicChoice(), new AlphaBeta(), board),
+                new AIPlayer(2, 4, new PointsEnemyDisadvantage(),  new ClassicChoice(), new AlphaBeta(), board)
             };
 
             if(players[0].GetType() == typeof(AIPlayer)) {
@@ -34,15 +36,18 @@ namespace SI3
                         players.ForEach(p => Console.Write($" gracz{p.Color}: {p.Points}"));
                         Console.Write("\n");
                         board.Print();
-
+                        Stopwatch sw = new Stopwatch();
+                        sw.Start();
                         Tuple<int, int> move = player.ChooseMove();
+                        sw.Stop();
+                        times += Int32.Parse( sw.ElapsedMilliseconds.ToString());
                         board.SetPoint(move.Item1, move.Item2, player.Color);
                         player.AddPoints(board.CalculatePointsGain(move.Item1, move.Item2));
                     }
                 }
             }
             List<Player> winners = players.Where(p => p.Points == players.Max(p2 => p2.Points)).ToList();
-
+            Console.WriteLine("Średni czas ruchu : " + (times / (board.size * board.size)));
             if(winners.Count > 1) {
                 Console.WriteLine("Remis!");
                 winners.ForEach(w => Console.WriteLine($"Gracz{w.Color} z liczbą punktów równą {w.Points}"));
